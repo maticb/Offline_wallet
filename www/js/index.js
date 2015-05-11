3e7ccc145c7c2a494c72b33d541794c1930181dc
maticb <matic.blagus@hotmail.com>
2015-05-12 01:45:47 +0200

    test qr

--- www/js/index.js
+++ www/js/index.js
@@ -2,22 +2,27 @@ var resultDiv;
 
 document.addEventListener("deviceready", init, false);
 function init() {
-    document.querySelector("#startScan").addEventListener("touchend", startScan, false);
-    resultDiv = document.querySelector("#results");
+    document.querySelector("#skeniraj_btn").addEventListener("touchend", startScan, false);
+    resultDiv = document.querySelector("#ls_test_o");
+
+    $("#ls_test_o").html(window.localStorage.getItem("ls_test"));
+    $("#ls_test_i").on('input', function () {
+        window.localStorage.setItem("ls_test", $("#ls_test_i").val());
+    });
 }
 
 function startScan() {
 
     cordova.plugins.barcodeScanner.scan(
-        function (result) {
-            var s = "Result: " + result.text + "<br/>" +
-            "Format: " + result.format + "<br/>" +
-            "Cancelled: " + result.cancelled;
-            resultDiv.innerHTML = s;
-        }, 
-        function (error) {
-            alert("Scanning failed: " + error);
-        }
+            function (result) {
+                var s = "Result: " + result.text + "<br/>" +
+                        "Format: " + result.format + "<br/>" +
+                        "Cancelled: " + result.cancelled;
+                resultDiv.innerHTML = s;
+            },
+            function (error) {
+                alert("Scanning failed: " + error);
+            }
     );
 
 }
