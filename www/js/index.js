2e4050da5213f28571ec6693e6e5ad331b379e33
maticb <matic.blagus@hotmail.com>
2015-05-20 16:09:14 +0200

    writer again

--- www/js/index.js
+++ www/js/index.js
@@ -19,111 +19,44 @@ function init() {
     document.querySelector("#output1").innerHTML = window.localStorage.getItem("ls_test");
 
 ///
+    document.addEventListener("deviceready", onDeviceReady, false);
 
 }
 
 //---------------------------------
 // retrieves root file system entry
-var getFileSystemRoot = (function () {
 
-    // private
-    var root;
+function onDeviceReady() {
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+}
 
-    // one-time retrieval of the root file system entry
-    var init = function () {
-        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0,
-                function (fileSystem) {
-                    root = fileSystem.root;
-                },
-                onFileSystemError);
+function gotFS(fileSystem) {
+    fileSystem.root.getFile("readme.txt", {create: true, exclusive: false}, gotFileEntry, fail);
+}
+
+function gotFileEntry(fileEntry) {
+    fileEntry.createWriter(gotFileWriter, fail);
+}
+
+function gotFileWriter(writer) {
+    writer.onwriteend = function (evt) {
+        console.log("contents of file now 'some sample text'");
+        writer.truncate(11);
+        writer.onwriteend = function (evt) {
+            console.log("contents of file now 'some sample'");
+            writer.seek(4);
+            writer.write(" different text");
+            writer.onwriteend = function (evt) {
+                console.log("contents of file now 'some different text'");
+            }
+        };
     };
-    document.addEventListener("deviceready", init, true);
-
-    // public function returns private root entry
-    return function () {
-        return root;
-    };
-}()); // execute immediately
-
-// file system error handler
-function onFileSystemError(error) {
-    var msg = 'file system error: ' + error.code;
-    navigator.notification.alert(msg, null, 'File System Error');
+    writer.write("some sample text");
 }
 
-// logs file events
-function onFileEvent(event) {
-    console.log('file event: ' + event.target.fileName + ' ' + event.type);
+function fail(error) {
+    console.log(error.code);
 }
-
-// called when error reading file
-function onFileError(event) {
-    console.log('file error: ' + event.target.error.code);
-}
-
-// called when file is written
-function onFileWrite(event) {
-    onFileEvent(event);
-    console.log('FileWriter position=' +
-            event.target.position + ", length=" + event.target.length);
-}
-
-// writes a text file to the device
-function writeFile() {
-    // root file system entry
-    var root = getFileSystemRoot(),
-            // writes a file
-            write_file = function (writer) {
-                var lineCount = 1;
-
-                // set the callbacks
-                writer.onwritestart = onFileEvent;
-                writer.onprogress = onFileEvent;
-                writer.onwrite = onFileWrite;
-                writer.onabort = onFileEvent;
-                writer.onerror = onFileError;
-                writer.onwriteend = function (event) {
-                    onFileEvent(event);
-                    lineCount += 1;
-                    if (lineCount <= 3) {
-                        // append a new line   
-                        writer.write('Line ' + lineCount + '.\r\n');
-                    }
-                    else {
-                        alert(writer.fileName +
-                                ' length=' + writer.length +
-                                ', position=' + writer.position);
-                    }
-                }
-
-                // append
-                writer.seek(writer.length);
-
-                // write to file
-                writer.write('Line ' + lineCount + '.\r\n');
-            },
-            // creates a FileWriter object
-            create_writer = function (fileEntry) {
-                fileEntry.createWriter(write_file, onFileSystemError);
-            };
-
-    // create a file and write to it
-    root.getFile('bbgap.txt', {create: true}, create_writer, onFileSystemError);
-}
-
-// remove file system entry
-function removeFile() {
-    var root = getFileSystemRoot();
-    var remove_file = function (entry) {
-        entry.remove(function () {
-            navigator.notification.alert(entry.toURI(), null, 'Entry deleted');
-        }, onFileSystemError);
-    };
-
-    // retrieve a file and truncate it
-    root.getFile('bbgap.txt', {create: false}, remove_file, onFileSystemError);
-}
-
 
 //---------------------------------
 function startScan() {
