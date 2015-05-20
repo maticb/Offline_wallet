4c26899ae64b922985f4a709a33dd154c4920037
maticb <matic.blagus@hotmail.com>
2015-05-20 13:13:22 +0200

    test qr create

--- www/js/index.js
+++ www/js/index.js
@@ -1,18 +1,25 @@
 var resultDiv;
 
+function wallet() {
+    this.uname = "";
+    this.pass = "";
+    this.coini = [];
+}
+
 document.addEventListener("deviceready", init, false);
 function init() {
     document.getElementById('startScan').addEventListener("touchend", startScan, false);
     document.getElementById('input').addEventListener("input", update_localstorage, false);
+    document.getElementById('qr_koda').addEventListener("touchend", qr_create, false);
     resultDiv = document.querySelector("#output");
-    
+
     document.querySelector("#output1").innerHTML = window.localStorage.getItem("ls_test");
 
 
 }
 
 function startScan() {
-    alert("FRAK OF");
+
     cordova.plugins.barcodeScanner.scan(
             function (result) {
                 var s = "Result: " + result.text + "<br/>" +
@@ -28,4 +35,16 @@ function startScan() {
 }
 function update_localstorage() {
     window.localStorage.setItem("ls_test", document.getElementById('input').value);
+}
+
+function qr_create()
+{
+    var qrcode = new QRCode("output2");
+
+    function makeCode() {
+
+        qrcode.makeCode("test");
+    }
+
+    makeCode();
 }
