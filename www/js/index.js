e3f72320a105f61554aca7172dfdccc572574da2
maticb <matic.blagus@hotmail.com>
2015-05-24 22:41:26 +0200

    if null

--- www/js/index.js
+++ www/js/index.js
@@ -24,7 +24,7 @@ function init() {
     //PC DEBUGGING
     //document.getElementById('saveWallet').addEventListener("click", saveWallet, false);
     //--
-
+    window.localStorage.setItem("ustvarjen", null);
     var ustvarjen = window.localStorage.getItem("ustvarjen");
     if (ustvarjen !== "da")//wallet ni ustvarjen
     {
@@ -35,8 +35,8 @@ function init() {
     }
     else
     {
-        denarnica = JSON.parse(window.localStorage.getItem("denarnica"));
-        alert(denarnica.uname +" ;");
+        denarnica = window.localStorage.getItem("denarnica");
+        alert(denarnica.uname + " ;");
     }
 }
 
@@ -75,7 +75,7 @@ function gotFile(fileEntry) {
             // document.querySelector("#output").innerHTML = this.result;
             var json = this.result;
             denarnica = JSON.parse(json);
-            window.localStorage.setItem("denarnica", json);
+            window.localStorage.setItem("denarnica", denarnica);
             window.localStorage.setItem("ustvarjen", "da");
 
             $("#container").css("display", "none");
@@ -216,7 +216,7 @@ function confirmNewWallet() {
     {
         denarnica.uname = uname;
         denarnica.pass = md5(pass);
-        window.localStorage.setItem("denarnica", JSON.stringify(denarnica));
+        window.localStorage.setItem("denarnica", denarnica);
         window.localStorage.setItem("ustvarjen", "da");
         hide_popup();
     }
